8d25dff15fca4a0849aa5e3364e47b48993f7693
Liverfidel <liverfidel01@gmail.com>
2023-12-08 20:00:59 -0400

    atu

--- lib/components/user_tile.dart
+++ lib/components/user_tile.dart
@@ -1,37 +1,65 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_crud/models/user.dart';
+import 'package:flutter_crud/providers/users.dart';
+import 'package:flutter_crud/routes/app_routes.dart';
+import 'package:provider/provider.dart';
 
 class UserTile extends StatelessWidget {
-  User user;
+  final User user;
 
-  UserTile({
-    super.key,
-    required this.user,
-    });
+  const UserTile(this.user);
 
   @override
   Widget build(BuildContext context) {
-    CircleAvatar ca;
-    if(user.avatarURL.isEmpty){
-      ca = CircleAvatar(child: Icon(Icons.person),);
-    }else{
-      ca = CircleAvatar(backgroundImage: NetworkImage(user.avatarURL),);
-    }
+    final avatar = user.avatarURL == null || user.avatarURL.isEmpty
+        ? CircleAvatar(child: Icon(Icons.person))
+        : CircleAvatar(backgroundImage: NetworkImage(user.avatarURL));
+
     return ListTile(
-      leading: ca,
+      leading: avatar,
       title: Text(user.nome),
       subtitle: Text(user.email),
       trailing: Container(
         width: 100,
         child: Row(
-          children: [
-            IconButton(onPressed: (){
-              
-            }, icon: Icon(Icons.edit), color: Color.fromARGB(255, 10, 141, 114),),
-            IconButton(onPressed: (){
-
-            }, icon: Icon(Icons.delete), color: Colors.red,),
+          children: <Widget>[
+            IconButton(
+              icon: Icon(Icons.edit),
+              color: Colors.orange,
+              onPressed: () {
+                Navigator.of(context).pushNamed(
+                  AppRoutes.USER_FORM,
+                  arguments: user,
+                );
+              },
+            ),
+            IconButton(
+              icon: const Icon(Icons.delete),
+              color: Colors.red,
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder: (ctx) => AlertDialog(
+                    title: Text('Excluir Usuário'),
+                    content: Text('Tem certeza?'),
+                    actions: <Widget>[
+                      TextButton(
+                        child: Text('Não'),
+                        onPressed: () => Navigator.of(context).pop(false),
+                      ),
+                      TextButton(
+                        child: Text('Sim'),
+                        onPressed: () => Navigator.of(context).pop(true),
+                      ),
+                    ],
+                  ),
+                ).then((confirmed) {
+                  if (confirmed) {
+                    Provider.of<Users>(context, listen: false).remove(user);
+                  }
+                });
+              },
+            ),
           ],
         ),
       ),
--- lib/providers/users.dart
+++ lib/providers/users.dart
@@ -1,11 +1,14 @@
 
-import 'dart:math';
+
+import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_crud/data/dummy_users.dart';
 import 'package:flutter_crud/models/user.dart';
+import 'package:http/http.dart' as http;
 
-class Users with ChangeNotifier{
+class Users extends ChangeNotifier {
+  static const _baseUrl = 'https://coder-firebase-teste-ff0a2-default-rtdb.firebaseio.com/';
   Map<String, User> _items = {...DUMMY_USERS};
 
   List<User> get all {
@@ -16,42 +19,55 @@ class Users with ChangeNotifier{
     return _items.length;
   }
 
-  User byIndex(int indice) {
-    return _items.values.elementAt(indice);
+  User byIndex(int i) {
+    return _items.values.elementAt(i);
   }
 
-  //cadastrar//
+  Future <void> put(User user) async {
+    if(user == null) {
+      return;
+    }
 
-  void put(User user){
-
-    if(user.id.trim().isNotEmpty && _items.containsKey(user.id) ){
-      //update
+    if(user.id != null && user.id.trim().isNotEmpty && _items.containsKey(user.id)) {
       _items.update(user.id, (_) => User(
         id: user.id,
         nome: user.nome,
         email: user.email,
         avatarURL: user.avatarURL,
-      ),
-    );
+        
+      )
+      );
     }
-    else{
-      final id = Random().nextDouble().toString();
-    
-    _items.putIfAbsent(
-      id,
-      () => User(
-        id: id, 
-        nome: user.nome, 
-        email: user.email, 
-        avatarURL: user.avatarURL,),
-    );
+    else {
+
+      final response = await http.post(Uri.parse("$_baseUrl/users.json"),
+  body: json.encode({
+    'nome': user.nome,
+    'email': user.email,
+    'avatarURL': user.avatarURL
+  }),
+);
+
+final responseBody = json.decode(response.body);
+if (responseBody != null && responseBody.containsKey('nome')) {
+  final id = responseBody['nome'];
+  print(responseBody);
+  _items.putIfAbsent(id, () => User(
+    id: id,
+    nome: user.nome,
+    email: user.email,
+    avatarURL: user.avatarURL,
+  ));
+}
+    notifyListeners();
   }
 
-    }
-    
+}
 
-  @override
-  void notifyListeners();
-  
-  
+  void remove(User user) {
+     if (user != null && user.id != null) {
+      _items.remove(user.id);
+      notifyListeners();
+    }
+  }
 }
